5ae7501cec217114fffceecef16dde967e7102da
ismaro3 <ismaro.394@gmail.com>
2016-07-21 19:48:12 +0200

    Completed dbpedia info

---
+++ src/main/java/Hurricane.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ismaro3 on 21/07/16.
+ */
+public class Hurricane {
+
+    public String URI;
+    public String abstract_;
+    public String areas;
+
+    public Hurricane(){
+        URI = "";
+        abstract_ = "";
+        areas = "";
+    }
+}
