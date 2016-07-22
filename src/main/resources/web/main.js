3c99689c2487d34c4f139b0f48fcab7de625f417
Borja A. Espejo-Garcia <borja3790@gmail.com>
2016-07-22 08:26:32 +0200

    removing markers when requerying

--- src/main/resources/web/main.js
+++ src/main/resources/web/main.js
@@ -1,19 +1,28 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-var earth;
-var isMicrophoneActive = true;
+    var earth;
+    var isMicrophoneActive = true;
 
-function flyToJapan() {
-        earth.fitBounds([[22, 122], [48, 154]]);
-        earth.panInsideBounds([[22, 122], [48, 154]],
-                            {heading: 90, tilt: 25, duration: 1});
-}
+    function flyToJapan() {
+        earth.fitBounds([
+            [22, 122],
+            [48, 154]
+        ]);
+        earth.panInsideBounds([
+            [22, 122],
+            [48, 154]
+        ], {
+            heading: 90,
+            tilt: 25,
+            duration: 1
+        });
+    }
 
-$("#move").click(function(){
- flyToJapan();
-});
+    $("#move").click(function() {
+        flyToJapan();
+    });
 
-        function initialize() {
+    function initialize() {
         earth = new WE.map('earth_div');
         earth.setView([46.8011, 8.2266], 2);
         /*WE.tileLayer('http://data.webglearth.com/natural-earth-color/{z}/{x}/{y}.jpg', {
@@ -27,57 +36,63 @@ $("#move").click(function(){
         WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
 
         // voice recognition
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    $('activateMicrophone').click(function(){
-       if (isMicrophoneActive) isMicrophoneActive = false;
-       else isMicrophoneActive = true;
-    });
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        $('activateMicrophone').click(function() {
+            if (isMicrophoneActive) isMicrophoneActive = false;
+            else isMicrophoneActive = true;
+        });
 
-var markers = [];
-$("input").on("keydown",function search(e) {
-    if(e.keyCode == 13) {
-        markers = [];
-        var path = "path?name=" + $("input").val() + "&season=2005";
-                        $.get( path, function( data ) {
-                            console.log(data);
-                          for (var i = 0; i < data.length; i++){
-                            var marker = WE.marker([data[i].lat, data[i].lon]).addTo(earth);
-                            markers.push(marker);
-                            marker.bindPopup("<b>"+ data[i].name +"</b>", {maxWidth: 150, closeButton: true});
-                          }
-                        });
-    }
-});
- 
-    recognition.onresult = function (e) {
-        if (isMicrophoneActive){
-        markers = [];
-        var textarea = document.getElementById('keywords');
-        textarea.value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                textarea.value += e.results[i][0].transcript;
-                console.log(e.results[i][0].transcript);
-                var path = "path?name=" + e.results[i][0].transcript + "&season=2005";
-                $.get( path, function( data ) {
+        var markers = [];
+        $("input").on("keydown", function search(e) {
+            if (e.keyCode == 13) {
+                for (var i = 0; i < markers.length; i++)
+                    earth.removeMarker(markers[i]);
+                var path = "path?name=" + $("input").val() + "&season=2005";
+                $.get(path, function(data) {
                     console.log(data);
-                  for (var i = 0; i < data.length; i++){
-                    var marker = WE.marker([data[i].lat, data[i].lon]).addTo(earth);
-                    markers.push(marker);
-                    marker.bindPopup("<b>"+ data[i].name +"</b>", {maxWidth: 150, closeButton: true});
-                  }
+                    for (var i = 0; i < data.length; i++) {
+                        var marker = WE.marker([data[i].lat, data[i].lon]).addTo(earth);
+                        markers.push(marker);
+                        marker.bindPopup("<b>" + data[i].name + "</b>", {
+                            maxWidth: 150,
+                            closeButton: true
+                        });
+                    }
                 });
             }
-        }
-      }
-    }
-    // start listening
+        });
+
+        recognition.onresult = function(e) {
+                if (isMicrophoneActive) {
+                    for (var i = 0; i < markers.length; i++)
+                        earth.removeMarker(markers[i]);
+                    var textarea = document.getElementById('keywords');
+                    textarea.value = "";
+                    for (var i = e.resultIndex; i < e.results.length; ++i) {
+                        if (e.results[i].isFinal) {
+                            textarea.value += e.results[i][0].transcript;
+                            console.log(e.results[i][0].transcript);
+                            var path = "path?name=" + e.results[i][0].transcript + "&season=2005";
+                            $.get(path, function(data) {
+                                console.log(data);
+                                for (var i = 0; i < data.length; i++) {
+                                    var marker = WE.marker([data[i].lat, data[i].lon]).addTo(earth);
+                                    markers.push(marker);
+                                    marker.bindPopup("<b>" + data[i].name + "</b>", {
+                                        maxWidth: 150,
+                                        closeButton: true
+                                    });
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+            // start listening
         recognition.start();
     }
 
     initialize();
 });
-
-
